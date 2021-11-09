f258c5eb80f8c160000df76b3948c3188132639f
Dzmitry Tsykman <dimatsikman@gmail.com>
2021-11-09 16:14:44 +0300

    Add project files.

---
+++ RootFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RootFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
