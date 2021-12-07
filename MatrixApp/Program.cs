8f8f208ba8c0c1c8090e379b86a7aa6128027220
Dzmitry Tsykman <dimatsikman@gmail.com>
2021-12-07 19:27:38 +0300

    Add project files.

---
+++ MatrixApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
