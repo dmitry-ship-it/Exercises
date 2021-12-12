bf6d5f47241cb31d8d0bb3d809937b4f0960fc86
Dzmitry Tsykman <dimatsikman@gmail.com>
2021-12-12 16:07:13 +0300

    Implemented Matrix class.

--- MatrixApp/Matrix.cs
+++ MatrixApp/Matrix.cs
@@ -12,32 +12,22 @@ namespace MatrixApp
 
         public Matrix(int rows, int cols)
         {
-            if (rows <= 0)
+            if (rows <= 0 || cols <= 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(rows), "Rows count cannot be less or equals zero.");
-            }
-
-            if (cols <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(cols), "Cols count cannot be less or equals zero.");
+                throw new ArgumentException("Rows and cols count cannot be less or equal zero.");
             }
 
             _elements = new double[rows, cols];
         }
 
-        public Matrix(int rows, int cols, double[,] elements) : this(rows, cols)
+        public Matrix(double[,] elements)
         {
             if (elements is null || elements.Length < 1)
             {
                 throw new ArgumentNullException(nameof(elements));
             }
 
-            if (elements.Length / rows != cols)
-            {
-                throw new ArgumentException("Цrong number of elements.", nameof(elements));
-            }
-
-            _elements = elements;
+            _elements = elements.Clone() as double[,];
         }
 
         public double this[int i, int j]
@@ -48,66 +38,51 @@ namespace MatrixApp
 
         public static Matrix Add(Matrix lhs, Matrix rhs)
         {
-            if (!IsEqualDimensions(lhs, rhs))
-            {
-                throw new ArgumentException("Matrix sizes do not match.");
-            }
-
-            var result = new Matrix(lhs.Rows, rhs.Cols);
-
-            for (var i = 0; i < lhs.Rows; i++)
-            {
-                for (var j = 0; j < lhs.Cols; j++)
-                {
-                    result[i, j] = lhs[i, j] + rhs[i, j];
-                }
-            }
-
-            return result;
-        }
-
-        public static Matrix operator +(Matrix lhs, Matrix rhs)
-        {
-            return Add(lhs, rhs);
+            return lhs + rhs;
         }
 
         public static Matrix Subtract(Matrix lhs, Matrix rhs)
         {
-            if (!IsEqualDimensions(lhs, rhs))
-            {
-                throw new ArgumentException("Matrix sizes do not match.");
-            }
-
-            var result = new Matrix(lhs.Rows, rhs.Rows);
-
-            for (var i = 0; i < lhs.Rows; i++)
-            {
-                for (var j = 0; j < lhs.Cols; j++)
-                {
-                    result[i, j] = lhs[i, j] - rhs[i, j];
-                }
-            }
-
-            return result;
-        }
-
-        public static Matrix operator -(Matrix lhs, Matrix rhs)
-        {
-            return Subtract(lhs, rhs);
+            return lhs - rhs;
         }
 
         public static Matrix Multiply(Matrix lhs, Matrix rhs)
         {
-            //TODO: implement matrix multiply
-            throw new NotImplementedException();
+            return lhs * rhs;
+        }
+
+        public static Matrix operator +(Matrix lhs, Matrix rhs)
+        {
+            return MatricesOperation(lhs, rhs, Operation.Add);
+        }
+
+        public static Matrix operator -(Matrix lhs, Matrix rhs)
+        {
+            return MatricesOperation(lhs, rhs, Operation.Subtract);
         }
 
         public static Matrix operator *(Matrix lhs, Matrix rhs)
         {
-            return Multiply(lhs, rhs);
+            if (lhs.Cols != rhs.Rows)
+            {
+                throw new ArgumentException(
+                    "The number of columns in the first matrix does not match the number of rows in the second matrix.");
+            }
+
+            var result = new double[lhs.Rows, rhs.Cols];
+
+            for (var i = 0; i < lhs.Rows; i++)
+            {
+                for (var j = 0; j < rhs.Cols; j++)
+                {
+                    result[i, j] = MultiplyMatricesCell(lhs, rhs, i, j);
+                }
+            }
+
+            return new Matrix(result);
         }
 
-        public static bool IsEqualDimensions(Matrix lhs, Matrix rhs)
+        public static bool IsEqualSizes(Matrix lhs, Matrix rhs)
         {
             return lhs.Rows == rhs.Rows
                 && lhs.Cols == rhs.Cols;
@@ -117,17 +92,59 @@ namespace MatrixApp
         {
             var result = new StringBuilder();
 
-            for (var i = 0; i < Rows; i++)
+            for (var row = 0; row < Rows; row++)
             {
-                for (var j = 0; j < Cols; j++)
-                {
-                    result.AppendFormat("{0,-8}", _elements[i, j]);
-                }
-
+                AppendRow(result, row);
                 result.Append(Environment.NewLine);
             }
 
             return result.ToString();
+        }
+
+        private void AppendRow(StringBuilder result, int row)
+        {
+            for (var j = 0; j < Cols; j++)
+            {
+                result.AppendFormat("{0,-8} ", _elements[row, j]);
+            }
+        }
+
+        private static double MultiplyMatricesCell(Matrix lhs, Matrix rhs, int row, int col)
+        {
+            var cell = 0d;
+
+            for (var i = 0; i < rhs.Rows; i++)
+            {
+                cell += lhs[row, i] * rhs[i, col];
+            }
+
+            return cell;
+        }
+
+        private enum Operation
+        {
+            Add = 1,
+            Subtract = -1
+        }
+
+        private static Matrix MatricesOperation(Matrix lhs, Matrix rhs, Operation operation)
+        {
+            if (!IsEqualSizes(lhs, rhs))
+            {
+                throw new ArgumentException("Matrix dimensions do not match.");
+            }
+
+            var result = new double[lhs.Rows, rhs.Cols];
+
+            for (var i = 0; i < lhs.Rows; i++)
+            {
+                for (var j = 0; j < lhs.Cols; j++)
+                {
+                    result[i, j] = lhs[i, j] + ((int)operation * rhs[i, j]);
+                }
+            }
+
+            return new Matrix(result);
         }
     }
 }
--- MatrixApp/Program.cs
+++ MatrixApp/Program.cs
@@ -6,11 +6,22 @@ namespace MatrixApp
     {
         private static void Main(string[] args)
         {
-            Matrix matrix1 = new(3, 4, new double[,] { { 1, 2, 3,1 }, { 4, 5, 6,1 }, { 6, 7, 8,1 } });
-            Matrix matrix2 = new(3, 3, new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 6, 7, 8 } });
+            var arr1 = new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 6, 7, 8 } };
+
+            Matrix matrix1 = new(arr1);
+            Matrix matrix2 = new(arr1);
 
             Console.WriteLine($"rows = {matrix1.Rows}, cols = {matrix1.Cols}");
             Console.WriteLine(matrix1 - matrix2);
+
+            matrix1[0, 0] = 33;
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(matrix1);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(matrix2);
         }
     }
 }
