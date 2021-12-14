07c496a9bbd81ea8a9ed3f1376997109720e417f
Dzmitry Tsykman <dimatsikman@gmail.com>
2021-12-14 15:00:58 +0300

    Add signaling mechanism for SignalStream.

---
+++ SignalStream/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SignalStream
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var signalStream = new SignalStream(File.OpenRead("file.txt"));
+            signalStream.TenthPartRead += (_, e) => Console.WriteLine($"{e.Percent}% read.");
+
+
+            byte[] buffer = new byte[signalStream.Length];
+            signalStream.Read(buffer, 0, (int)signalStream.Length);
+
+            Console.WriteLine(Encoding.Default.GetString(buffer));
+        }
+    }
+}
---
+++ SignalStream/SignalStream.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace SignalStream
+{
+    internal class SignalStream : Stream
+    {
+        private readonly Stream _stream;
+
+        public SignalStream(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public event EventHandler<ReadPercentEventArgs> TenthPartRead;
+
+        public override bool CanRead => _stream.CanRead;
+
+        public override bool CanSeek => _stream.CanSeek;
+
+        public override bool CanWrite => _stream.CanWrite;
+
+        public override long Length => _stream.Length;
+
+        public override long Position { get => _stream.Position; set => _stream.Position = value; }
+
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var bytesRead = 0;
+            var persentStep = (int)Math.Round(_stream.Length / 10d);
+
+            var length = count > buffer.Length
+                ? buffer.Length
+                : count;
+            for (var i = offset; i < length; i += persentStep)
+            {
+                if (i + persentStep > _stream.Length)
+                {
+                    bytesRead += _stream.Read(buffer, i, (int)_stream.Length - i);
+                    break;
+                }
+
+                bytesRead += _stream.Read(buffer, i, persentStep);
+
+                TenthPartRead?.Invoke(this, new ReadPercentEventArgs() { Percent = bytesRead / persentStep * 10 });
+            }
+
+            return bytesRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            _stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _stream.Write(buffer, offset, count);
+        }
+
+        protected virtual void OnTenthPartRead(ReadPercentEventArgs e)
+        {
+            TenthPartRead?.Invoke(this, e);
+        }
+    }
+}
