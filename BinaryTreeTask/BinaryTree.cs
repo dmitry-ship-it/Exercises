aadbd0ea1adbe56fdcd18d3820469c6b1dcc4beb
Dzmitry Tsykman <dimatsikman@gmail.com>
2021-12-20 19:21:58 +0300

    Add project files.

---
+++ BinaryTreeTask/BinaryTree.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTreeTask
+{
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private sealed class Node : IEnumerable<T>
+        {
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public T Value { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                if (Left is not null)
+                {
+                    foreach (var value in Left)
+                    {
+                        yield return value;
+                    }
+                }
+
+                yield return Value;
+
+                if (Right is not null)
+                {
+                    foreach (var value in Right)
+                    {
+                        yield return value;
+                    }
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        private Node _root;
+
+        public void Insert(T value)
+        {
+            if (_root is null)
+            {
+                _root = new Node(value);
+                return;
+            }
+
+            InsertHelper(_root, new Node(value));
+        }
+
+        private void InsertHelper(Node parent, Node newNode)
+        {
+            if (parent is null)
+            {
+                parent = newNode;
+            }
+
+            if (newNode.Value.CompareTo(parent.Value) < 0)
+            {
+                if (parent.Left is null)
+                {
+                    parent.Left = newNode;
+                }
+                else
+                {
+                    InsertHelper(parent.Left, newNode);
+                }
+            }
+            else
+            {
+                if (parent.Right is null)
+                {
+                    parent.Right = newNode;
+                }
+                else
+                {
+                    InsertHelper(parent.Right, newNode);
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _root.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        // use only for debug
+        // TODO: Delete after task completion
+        #region RemoveThis
+        private void DisplayTree(Node root)
+        {
+            if (root is not null)
+            {
+                DisplayTree(root.Left);
+                Console.Write($"{root.Value} ");
+                DisplayTree(root.Right);
+            }
+        }
+
+        public void DisplayTree()
+        {
+            DisplayTree(_root);
+        }
+        #endregion
+    }
+}
---
+++ BinaryTreeTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace BinaryTreeTask
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            var tree = new BinaryTree<string>();
+
+            for (var i = 0; i < 7405; i++)
+            {
+                tree.Insert("some string");
+            }
+
+            var timer = Stopwatch.StartNew();
+            foreach (var item in tree)
+            {
+                Console.Write($"{item} ");
+            }
+            timer.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"Using yield time = {TimeSpan.FromTicks(timer.ElapsedTicks).TotalMilliseconds}ms");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var timer2 = Stopwatch.StartNew();
+            tree.DisplayTree();
+            timer2.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"Using recursion time = {TimeSpan.FromTicks(timer2.ElapsedTicks).TotalMilliseconds}ms");
+        }
+    }
+}
